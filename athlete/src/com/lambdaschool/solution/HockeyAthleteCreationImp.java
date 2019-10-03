636234b285a041109e581dda9447c50f60060f2a
rndpatel@gmail.com <rndpatel@gmail.com>
2019-10-03 13:57:32 -0700

    mvp complete - impl/injector's made

---
+++ athlete/src/com/lambdaschool/solution/HockeyAthleteCreationImp.java
@@ -0,0 +1,8 @@
+package com.lambdaschool.solution;
+
+public class HockeyAthleteCreationImp implements AthleteCreation {
+    @Override
+    public void displayAthlete() {
+        System.out.println("Hockey Athlete");
+    }
+}
